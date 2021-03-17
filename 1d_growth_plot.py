a206ea071b9e741a0cbe28ef04bba16f96681579
Sherwood Richers <srichers@berkeley.edu>
2021-03-17 08:12:53 -0700

    made growth rate plot have two panels to show growth of the N and F modes separately

--- 1d_growth_plot.py
+++ 1d_growth_plot.py
@@ -58,51 +58,54 @@ mpl.rcParams['axes.linewidth'] = 2
 # MAKE PLOT #
 #############
 
-fig, ax = plt.subplots(1,1, figsize=(6,5))
+fig, axes = plt.subplots(2,1, figsize=(6,10))
+plt.subplots_adjust(hspace=0,wspace=0)
 
-ylabel = r"$\langle |Q|\rangle /\mathrm{Tr}(n_{ab})$"
-ax.set_ylabel(ylabel)
-ax.set_xlabel(r"$t\,(10^{-9}\,\mathrm{s})$")    
-ax.tick_params(axis='both', which='both', direction='in', right=True,top=True)
-ax.xaxis.set_minor_locator(AutoMinorLocator())
-ax.yaxis.set_minor_locator(AutoMinorLocator())
-ax.set_yscale("log")
-ax.set_ylim(1e-12,.99)
-ax.yaxis.set_major_locator(LogLocator(base=10,numticks=20))
-ax.yaxis.set_minor_locator(LogLocator(base=10,subs = np.arange(1,10)*.1,numticks=40))
+axes[0].set_ylabel(r"$\langle |n_{ab}|\rangle /\mathrm{Tr}(n_{ab})$")
+axes[1].set_ylabel(r"$\langle |\mathbf{f}^{(x)}_{ab}|\rangle /\mathrm{Tr}(n_{ab})$")
+axes[1].set_xlabel(r"$t\,(10^{-9}\,\mathrm{s})$")
+axes[0].set_xticklabels([])
 
-xlim = (0,0.5) # ns
+for ax in axes:
+    ax.tick_params(axis='both', which='both', direction='in', right=True,top=True)
+    ax.xaxis.set_minor_locator(AutoMinorLocator())
+    ax.yaxis.set_minor_locator(AutoMinorLocator())
+    ax.set_yscale("log")
+    ax.set_ylim(1e-12,.99)
+    ax.yaxis.set_major_locator(LogLocator(base=10,numticks=20))
+    ax.yaxis.set_minor_locator(LogLocator(base=10,subs = np.arange(1,10)*.1,numticks=40))
+    xlim = (0,0.5) # ns
+    ax.set_xlim(xlim)
 
-ax.set_xlim(xlim)
 if args.antineutrinos:
-    ax.plot(t,    Nbar[:,0,1],color="purple",label=r"$\bar{n}_{e\mu}$")
-    ax.plot(t,  Fbar[:,0,0,1],color="purple",label=r"$\mathbf{\bar{f}}^{(x)}_{e\mu}$",linestyle="--")
-    ax.plot(t,    Nbar[:,0,2],color="green" ,label=r"$\bar{n}_{e\tau}$")
-    ax.plot(t,  Fbar[:,0,0,2],color="green" ,label=r"$\mathbf{\bar{f}}^{(x)}_{e\tau}$",linestyle="--")
-    ax.plot(t,    Nbar[:,1,2],color="orange",label=r"$\bar{n}_{\mu\tau}$")
-    ax.plot(t,  Fbar[:,0,1,2],color="orange",label=r"$\mathbf{\bar{f}}^{(x)}_{\mu\tau}$",linestyle="--")
+    axes[0].plot(t,    Nbar[:,0,1],color="purple",label=r"$\bar{n}_{e\mu}$")
+    axes[1].plot(t,  Fbar[:,0,0,1],color="purple",label=r"$\mathbf{\bar{f}}^{(x)}_{e\mu}$")
+    axes[0].plot(t,    Nbar[:,0,2],color="green" ,label=r"$\bar{n}_{e\tau}$")
+    axes[1].plot(t,  Fbar[:,0,0,2],color="green" ,label=r"$\mathbf{\bar{f}}^{(x)}_{e\tau}$")
+    axes[0].plot(t,    Nbar[:,1,2],color="orange",label=r"$\bar{n}_{\mu\tau}$")
+    ax.plot(t,  Fbar[:,0,1,2],color="orange",label=r"$\mathbf{\bar{f}}^{(x)}_{\mu\tau}$")
     if args.include_mumu:
-        ax.plot(t,    Nbar[:,1,1],color="brown",label=r"$\bar{n}_{\mu\mu}$")
-        ax.plot(t,  Fbar[:,0,1,1],color="brown",label=r"$\mathbf{\bar{f}}^{(x)}_{\mu\mu}$",linestyle="--")
+        axes[0].plot(t,    Nbar[:,1,1],color="brown",label=r"$\bar{n}_{\mu\mu}$",linestyle="--")
+        axes[1].plot(t,  Fbar[:,0,1,1],color="brown",label=r"$\mathbf{\bar{f}}^{(x)}_{\mu\mu}$",linestyle="--")
     if args.include_tautau:
-        ax.plot(t,    Nbar[:,2,2],color="salmon",label=r"$\bar{n}_{\tau\tau}$")
-        ax.plot(t,  Fbar[:,0,2,2],color="salmon",label=r"$\mathbf{\bar{f}}^{(x)}_{\tau\tau}$",linestyle="--")
+        axes[0].plot(t,    Nbar[:,2,2],color="salmon",label=r"$\bar{n}_{\tau\tau}$",linestyle="--")
+        axes[1].plot(t,  Fbar[:,0,2,2],color="salmon",label=r"$\mathbf{\bar{f}}^{(x)}_{\tau\tau}$",linestyle="--")
 else:
-    ax.plot(t,    N[:,0,1],color="purple",label=r"$n_{e\mu}$")
-    ax.plot(t,  F[:,0,0,1],color="purple",label=r"$\mathbf{f}^{(x)}_{e\mu}$",linestyle="--")
-    ax.plot(t,    N[:,0,2],color="green" ,label=r"$n_{e\tau}$")
-    ax.plot(t,  F[:,0,0,2],color="green" ,label=r"$\mathbf{f}^{(x)}_{e\tau}$",linestyle="--")
-    ax.plot(t,    N[:,1,2],color="orange",label=r"$n_{\mu\tau}$")
-    ax.plot(t,  F[:,0,1,2],color="orange",label=r"$\mathbf{f}^{(x)}_{\mu\tau}$",linestyle="--")
+    axes[0].plot(t,    N[:,0,1],color="purple",label=r"$e\mu$")
+    axes[1].plot(t,  F[:,0,0,1],color="purple",label=r"$e\mu$")
+    axes[0].plot(t,    N[:,0,2],color="green" ,label=r"$e\tau$")
+    axes[1].plot(t,  F[:,0,0,2],color="green" ,label=r"$e\tau$")
+    axes[0].plot(t,    N[:,1,2],color="orange",label=r"$\mu\tau$")
+    axes[1].plot(t,  F[:,0,1,2],color="orange",label=r"$\mu\tau$")
     if args.include_mumu:
-        ax.plot(t,    N[:,1,1],color="brown",label=r"$n_{\mu\mu}$")
-        ax.plot(t,  F[:,0,1,1],color="brown",label=r"$\mathbf{f}^{(x)}_{\mu\mu}$",linestyle="--")
+        axes[0].plot(t,    N[:,1,1],color="red",label=r"$\mu\mu$",linestyle="--")
+        axes[1].plot(t,  F[:,0,1,1],color="red",label=r"$\mu\mu$",linestyle="--")
     if args.include_tautau:
-        ax.plot(t,    N[:,2,2],color="salmon",label=r"$n_{\tau\tau}$")
-        ax.plot(t,  F[:,0,2,2],color="salmon",label=r"$\mathbf{f}^{(x)}_{\tau\tau}$",linestyle="--")
+        axes[0].plot(t,    N[:,2,2],color="gold",label=r"$\tau\tau$",linestyle="--")
+        axes[1].plot(t,  F[:,0,2,2],color="gold",label=r"$\tau\tau$",linestyle="--")
 
-if args.include_mumu or args.include_tautau:
-    lines = ax.get_lines()
+if False: #args.include_mumu or args.include_tautau:
+    lines = axes[0].get_lines()
     n_lines = []
     f_lines = []
 
@@ -113,10 +116,10 @@ if args.include_mumu or args.include_tautau:
             f_lines.append(line)
 
     n_legend = plt.legend(n_lines, [l.get_label() for l in n_lines], bbox_to_anchor=(1.0, 0.55), loc="lower right", frameon=False, ncol=2, fontsize=18, handlelength=0.8, handletextpad=0.25, columnspacing=.7)
-    f_legend = plt.legend(f_lines, [l.get_label() for l in f_lines], loc="lower right", frameon=False, ncol=2, fontsize=18, handlelength=0.8, handletextpad=0.25, columnspacing=.7)
-    ax.add_artist(n_legend)
+    #f_legend = plt.legend(f_lines, [l.get_label() for l in f_lines], loc="lower right", frameon=False, ncol=2, fontsize=18, handlelength=0.8, handletextpad=0.25, columnspacing=.7)
+    axes[0].add_artist(n_legend)
 else:
-    ax.legend(frameon=False,ncol=5,fontsize=18,handlelength=.8,handletextpad=0.25,columnspacing=.7)
+    axes[0].legend(frameon=False,ncol=2,loc="center right",fontsize=18,handlelength=.8,handletextpad=0.25,columnspacing=.7)
 
 
 
@@ -129,9 +132,9 @@ t1=.25
 y0=5e-5
 growth = np.exp(w*(t1-t0)*1e-9)
 y1=y0 * growth
-ax.plot([t0,t1],[y0,y1],color="blue")
+axes[0].plot([t0,t1],[y0,y1],color="blue")
 #ax.plot([t0,t1],[y0/growth,y1],color="gray")
-ax.text(t0-.02,y0*1.5,r"$\mathrm{Im}(\omega)=6.50\times10^{10}\,\mathrm{s}^{-1}$",color="blue",rotation=44,fontsize=12)
+axes[0].text(t0-.02,y0*1.5,r"$\mathrm{Im}(\omega)=6.50\times10^{10}\,\mathrm{s}^{-1}$",color="blue",rotation=44,fontsize=12)
 
 w = 1.06e10
 t0=.1
@@ -139,9 +142,9 @@ t1=.25
 y0=8e-8
 y1=y0 * np.exp(w*(t1-t0)*1e-9)
 #print(w*(t1-t0)*1e-9)
-ax.plot([t0,t1],[y0,y1],color="red")
+axes[1].plot([t0,t1],[y0,y1],color="red")
 #ax.text(t0-.01,y0*1.7,r"$\mathrm{Im}(\omega)=1.06\times10^{10}\,\mathrm{s}^{-1}$",color="red",rotation=8,fontsize=12)
-ax.text(t0*1.9,y0*5.0,r"$\mathrm{Im}(\omega)=1.06\times10^{10}\,\mathrm{s}^{-1}$",color="red",rotation=8,fontsize=12)
+axes[1].text(t0-.02,y0*1.5,r"$\mathrm{Im}(\omega)=1.06\times10^{10}\,\mathrm{s}^{-1}$",color="red",rotation=8,fontsize=12)
 
 name = "1d_growth_plot"
 if args.include_mumu:
