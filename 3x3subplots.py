c9caf4540ed11540222b5a2dc119c684451c2665
Sherwood Richers <sarichers@gmail.com>
2021-03-17 20:37:45 -0400

    made the 3x3 plot script do a single simulation at a time to make it more generally usable

--- 3x3subplots.py
+++ 3x3subplots.py
@@ -91,96 +91,73 @@ plt.rcParams["font.serif"] = ["Times New Roman"] + plt.rcParams["font.serif"]
 
 yt.funcs.mylog.setLevel(100)
 
-#gathering all 2D pltfile directories
-directory_inputs = sorted(glob.glob("/ocean/projects/phy200048p/shared/2D/*/"))
-#looping through directory names and creating output plot directories corresponding to each input
-for d in directory_inputs:
-    print("currently checking directory: "+d)
-    #taking off the last "/" so I can do path.split
-    directory_short = d[:-1]
-    #grabbing current directory name
-    directory_current = os.path.split(directory_short)[-1]
-    #creating output sub-directory in plots directory
-    directory_out = "/ocean/projects/phy200048p/shared/plots/"+directory_current
-    #checking if the directory doesnt exist, or if it doesn't have any subplot files inside... if so, create the directory and do the subplot routine. If directory exists and has subplot files, skip it
-    if os.path.exists(directory_out) == True and len(glob.glob(directory_out+"/*3x3subplot.png")) != 0:
-        print("output directory already exists and contains 3x3 subplots")
-        continue
-    #If directory doesnt have subplot files in it, run the subplot routine
-    else:
-        print("output directory doesn't have 3x3 subplot files in it yet, running subplot script")
-        #if the directory doesn't exist, make it first
-        if not os.path.exists(directory_out):
-            print("output directory does not exist, creating directory")
-            os.makedirs(directory_out)
-        #grab all pltfiles for creating subplots
-        filenames = sorted(glob.glob(d+"plt*"))
-        for filename in filenames:
-            print(filename)
-            ds = yt.load(filename)
-            slc = ds.slice('x', 0.)
-            #use fixed resolution buffer to extract 2D slice data object information
-            frb = slc.to_frb(width = (ds.domain_right_edge[1].value - ds.domain_left_edge[1].value, 'cm'), height = (ds.domain_right_edge[2].value - ds.domain_left_edge[2].value, 'cm'), resolution = (ds.domain_dimensions[1], ds.domain_dimensions[2]))
-            #list of fields to plot ordered by row and then column
-            fields = [['N00_Mag','N01_Mag','N02_Mag'],['N01_Phase','N11_Mag','N12_Mag'],['N02_Phase','N12_Phase','N22_Mag']]
-            labels = [[r'$N_{ee}$',r'$N_{e\mu}$',r'$N_{e\tau}$'],[r'$\phi_{e\mu}$',r'$N_{\mu\mu}$',r'$N_{\mu\tau}$'],[r'$\phi_{e\tau}$',r'$\phi_{\mu\tau}$',r'$N_{\tau\tau}$']]
+filenames = sorted(glob.glob("plt*"))
+for filename in filenames:
+    print(filename)
+    ds = yt.load(filename)
+    slc = ds.slice('x', 0.)
+    #use fixed resolution buffer to extract 2D slice data object information
+    frb = slc.to_frb(width = (ds.domain_right_edge[1].value - ds.domain_left_edge[1].value, 'cm'), height = (ds.domain_right_edge[2].value - ds.domain_left_edge[2].value, 'cm'), resolution = (ds.domain_dimensions[1], ds.domain_dimensions[2]))
+    #list of fields to plot ordered by row and then column
+    fields = [['N00_Mag','N01_Mag','N02_Mag'],['N01_Phase','N11_Mag','N12_Mag'],['N02_Phase','N12_Phase','N22_Mag']]
+    labels = [[r'$N_{ee}$',r'$N_{e\mu}$',r'$N_{e\tau}$'],[r'$\phi_{e\mu}$',r'$N_{\mu\mu}$',r'$N_{\mu\tau}$'],[r'$\phi_{e\tau}$',r'$\phi_{\mu\tau}$',r'$N_{\tau\tau}$']]
+    
+    # Set up a 3x3 figure
+    fig, axes = plt.subplots(nrows=3, ncols=3, figsize=(15,15), sharex=True, sharey=True)
+    print("created figure")
+    #defining the two colorbars to be used
+    cax1 = fig.add_axes([.93, 0.122, 0.03, 0.76])
+    norm1 = colors.Normalize(vmin=0, vmax=0.6)
+    cmap = plt.get_cmap('viridis')
+    cb1 = colorbar.ColorbarBase(cax1, cmap=cmap,norm=norm1,orientation='vertical')
+    cb1.set_label(r'$N_{ij} / \mathrm{Tr}(N)$', fontsize=22)
+    cb1.ax.tick_params(axis='y', which='both', direction='in')
+    cb1.minorticks_on()
+    cax1.yaxis.set_major_locator(ticker.MultipleLocator(0.1))
+    cax1.yaxis.set_minor_locator(ticker.MultipleLocator(0.02))
+    
+    cax2 = fig.add_axes([0.04, 0.122, 0.03, 0.76])
+    norm2 = colors.Normalize(vmin=-180, vmax=180)
+    cmap2 = plt.get_cmap('twilight')
+    cb2 = colorbar.ColorbarBase(cax2, cmap=cmap2,norm=norm2,orientation='vertical')
+    cax2.yaxis.set_ticks_position('left')
+    cax2.yaxis.set_label_position('left')
+    cb2.set_label(r'$\phi_{ij}$ (degrees)', fontsize=22)
+    cb2.ax.tick_params(axis='y', which='both', direction='in')
+    cax2.minorticks_on()
+    cax2.yaxis.set_major_locator(ticker.MultipleLocator(45))
+    cax2.yaxis.set_minor_locator(ticker.MultipleLocator(5))
+    
+    for i in range(0,3):
+        for j in range(0,3):
+            ax = axes[i,j]
+            ax.minorticks_on()
+            ax.yaxis.set_major_locator(ticker.MultipleLocator(10))
+            ax.yaxis.set_minor_locator(ticker.MultipleLocator(2))
+            ax.xaxis.set_major_locator(ticker.MultipleLocator(10))
+            ax.xaxis.set_minor_locator(ticker.MultipleLocator(2))
+            ax.tick_params(axis='both', which='both', direction='in', right=True,top=True)
+            ax.text(0.1,0.9, "{}".format(labels[i][j]), fontsize=18, ha="center", va="center", transform=ax.transAxes, bbox=dict(facecolor='white', alpha=0.75, edgecolor='.75'))
+            #setting the phase plots
+            if ax == axes[1,0] or ax == axes[2,0] or ax == axes[2,1]:
+                im = ax.imshow(frb[fields[i][j]].d,origin='lower',extent=[ds.domain_left_edge[1].value,ds.domain_right_edge[1].value,ds.domain_left_edge[2].value,ds.domain_right_edge[2].value], vmin=-180, vmax=180, cmap='twilight')
+                #setting colorbar limits
+                im.set_clim(-180,180)
+                #setting the N magnitudes plots
+            else:
+                #origin=lower keeps image from getting flipped, extent gives the grid coordinates in physical units
+                im = ax.imshow(frb[fields[i][j]].d,origin='lower',extent=[ds.domain_left_edge[1].value,ds.domain_right_edge[1].value,ds.domain_left_edge[2].value,ds.domain_right_edge[2].value], vmin=0., vmax=0.6, cmap='viridis')
+                #setting colorbar limits
+                im.set_clim(0.,0.6)
+    for a in axes[-1,:]: a.set_xlabel('y (cm)')
+    for a in axes[:,0]: a.set_ylabel('z (cm)')
 
-            # Set up a 3x3 figure
-            fig, axes = plt.subplots(nrows=3, ncols=3, figsize=(15,15), sharex=True, sharey=True)
-            print("created figure")
-            #defining the two colorbars to be used
-            cax1 = fig.add_axes([.93, 0.122, 0.03, 0.76])
-            norm1 = colors.Normalize(vmin=0, vmax=0.6)
-            cmap = plt.get_cmap('viridis')
-            cb1 = colorbar.ColorbarBase(cax1, cmap=cmap,norm=norm1,orientation='vertical')
-            cb1.set_label(r'$N_{ij} / \mathrm{Tr}(N)$', fontsize=22)
-            cb1.ax.tick_params(axis='y', which='both', direction='in')
-            cb1.minorticks_on()
-            cax1.yaxis.set_major_locator(ticker.MultipleLocator(0.1))
-            cax1.yaxis.set_minor_locator(ticker.MultipleLocator(0.02))
-
-            cax2 = fig.add_axes([0.04, 0.122, 0.03, 0.76])
-            norm2 = colors.Normalize(vmin=-180, vmax=180)
-            cmap2 = plt.get_cmap('twilight')
-            cb2 = colorbar.ColorbarBase(cax2, cmap=cmap2,norm=norm2,orientation='vertical')
-            cax2.yaxis.set_ticks_position('left')
-            cax2.yaxis.set_label_position('left')
-            cb2.set_label(r'$\phi_{ij}$ (degrees)', fontsize=22)
-            cb2.ax.tick_params(axis='y', which='both', direction='in')
-            cax2.minorticks_on()
-            cax2.yaxis.set_major_locator(ticker.MultipleLocator(45))
-            cax2.yaxis.set_minor_locator(ticker.MultipleLocator(5))
-
-            for i in range(0,3):
-                for j in range(0,3):
-                    ax = axes[i,j]
-                    ax.minorticks_on()
-                    ax.yaxis.set_major_locator(ticker.MultipleLocator(10))
-                    ax.yaxis.set_minor_locator(ticker.MultipleLocator(2))
-                    ax.xaxis.set_major_locator(ticker.MultipleLocator(10))
-                    ax.xaxis.set_minor_locator(ticker.MultipleLocator(2))
-                    ax.tick_params(axis='both', which='both', direction='in', right=True,top=True)
-                    ax.text(0.1,0.9, "{}".format(labels[i][j]), fontsize=18, ha="center", va="center", transform=ax.transAxes, bbox=dict(facecolor='white', alpha=0.75, edgecolor='.75'))
-                    #setting the phase plots
-                    if ax == axes[1,0] or ax == axes[2,0] or ax == axes[2,1]:
-                        im = ax.imshow(frb[fields[i][j]].d,origin='lower',extent=[ds.domain_left_edge[1].value,ds.domain_right_edge[1].value,ds.domain_left_edge[2].value,ds.domain_right_edge[2].value], vmin=-180, vmax=180, cmap='twilight')
-                        #setting colorbar limits
-                        im.set_clim(-180,180)
-                    #setting the N magnitudes plots
-                    else:
-                        #origin=lower keeps image from getting flipped, extent gives the grid coordinates in physical units
-                        im = ax.imshow(frb[fields[i][j]].d,origin='lower',extent=[ds.domain_left_edge[1].value,ds.domain_right_edge[1].value,ds.domain_left_edge[2].value,ds.domain_right_edge[2].value], vmin=0., vmax=0.6, cmap='viridis')
-                        #setting colorbar limits
-                        im.set_clim(0.,0.6)
-            for a in axes[-1,:]: a.set_xlabel('y (cm)')
-            for a in axes[:,0]: a.set_ylabel('z (cm)')
-
-            #plt.tick_params(axis='both', which='both', direction='in', right=True,top=True)
-            #increases the spacing between subplots so axis labels don't overlap
-            plt.subplots_adjust(hspace=0.05,wspace=0.)
-            #fig.tight_layout()
-            #plt.show()
-            #fig.savefig(directory_out+"/"+"{0}_3x3subplot.pdf".format(ds),dpi=300, bbox_inches="tight")
-            fig.savefig(directory_out+"/"+"{0}_3x3subplot.png".format(ds),dpi=300, bbox_inches="tight")
-            print("successfully saved figure")
-            plt.close()
+    #plt.tick_params(axis='both', which='both', direction='in', right=True,top=True)
+    #increases the spacing between subplots so axis labels don't overlap
+    plt.subplots_adjust(hspace=0.05,wspace=0.)
+    #fig.tight_layout()
+    #plt.show()
+    #fig.savefig(directory_out+"/"+"{0}_3x3subplot.pdf".format(ds),dpi=300, bbox_inches="tight")
+    fig.savefig("{0}_3x3subplot.png".format(ds),dpi=300, bbox_inches="tight")
+    print("successfully saved figure")
+    plt.close()
