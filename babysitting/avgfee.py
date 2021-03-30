211dcd6f529ad32826aa37306dc3fe10c0923886
Sherwood Richers <sarichers@gmail.com>
2021-03-30 17:58:15 -0400

    added basic plotting script for use when babysitting runs

---
+++ babysitting/avgfee.py
@@ -0,0 +1,76 @@
+# Run from /ocean/projects/phy200048p/shared to generate plot showing time evolution of <fee> at different dimensionalities
+
+import numpy as np
+import matplotlib.pyplot as plt
+import glob
+import h5py
+import matplotlib as mpl
+from matplotlib.ticker import (MultipleLocator, FormatStrFormatter,AutoMinorLocator,LogLocator)
+
+
+base=["N","Fx","Fy","Fz"]
+diag_flavor=["00","11","22"]
+offdiag_flavor=["01","02","12"]
+re=["Re","Im"]
+# real/imag
+R=0
+I=1
+    
+
+def offdiagMag(f):
+    return np.sqrt(f[:,0,1,R]**2 + f[:,0,1,I]**2 +
+                   f[:,0,2,R]**2 + f[:,0,2,I]**2 +
+                   f[:,1,2,R]**2 + f[:,1,2,I]**2)
+
+
+######################
+# read averaged data #
+######################
+def plotdata(filename):
+    avgData = h5py.File(filename,"r")
+    t=np.array(avgData["t"])*1e9
+    N=np.array(avgData["N_avg_mag"])[:,0,0]
+    avgData.close()
+    return t, N
+
+################
+# plot options #
+################
+mpl.rcParams['font.size'] = 22
+mpl.rcParams['font.family'] = 'serif'
+#mpl.rc('text', usetex=True)
+mpl.rcParams['xtick.major.size'] = 7
+mpl.rcParams['xtick.major.width'] = 2
+mpl.rcParams['xtick.major.pad'] = 8
+mpl.rcParams['xtick.minor.size'] = 4
+mpl.rcParams['xtick.minor.width'] = 2
+mpl.rcParams['ytick.major.size'] = 7
+mpl.rcParams['ytick.major.width'] = 2
+mpl.rcParams['ytick.minor.size'] = 4
+mpl.rcParams['ytick.minor.width'] = 2
+mpl.rcParams['axes.linewidth'] = 2
+
+
+fig, ax = plt.subplots(1,1, figsize=(6,5))
+
+##############
+# formatting #
+##############
+ax.axhline(1./3., color="green")
+ax.set_ylabel(r"$\langle N_{ee}\rangle /\mathrm{Tr}(N)$")
+ax.set_xlabel(r"$t\,(10^{-9}\,\mathrm{s})$")
+ax.tick_params(axis='both', which='both', direction='in', right=True,top=True)
+ax.xaxis.set_minor_locator(AutoMinorLocator())
+ax.yaxis.set_minor_locator(AutoMinorLocator())
+
+#############
+# plot data #
+#############
+filename = "reduced_data.h5"
+t,N = plotdata(filename)
+ax.plot(t, N)
+
+############
+# save pdf #
+############
+plt.savefig("avgfee.pdf", bbox_inches="tight")
