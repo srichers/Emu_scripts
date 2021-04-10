19dec4b4429e4f040eb2abc96dc818802f07a05e
Sherwood Richers <srichers@caltech.edu>
2021-04-10 13:04:40 -0700

    optimize data reduction to get both average abundances and FFT quantities at the same time so both scripts dont have to be executed separately. Dominant cost is file loading anyway.

--- data_reduction/reduce_data.py
+++ data_reduction/reduce_data.py
@@ -12,6 +12,10 @@ import yt
 import glob
 import multiprocessing as mp
 import h5py
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+import emu_yt_module as emu
 
 # don't re-run if data already exists
 outputfilename = "reduced_data.h5"
@@ -27,6 +31,85 @@ base=["N","Fx","Fy","Fz"]
 diag_flavor=["00","11","22"]
 offdiag_flavor=["01","02","12"]
 re=["Re","Im"]
+
+N00_FFT = []
+N01_FFT = []
+N02_FFT = []
+N11_FFT = []
+N12_FFT = []
+N22_FFT = []
+
+Fx00_FFT = []
+Fx01_FFT = []
+Fx02_FFT = []
+Fx11_FFT = []
+Fx12_FFT = []
+Fx22_FFT = []
+
+Fy00_FFT = []
+Fy01_FFT = []
+Fy02_FFT = []
+Fy11_FFT = []
+Fy12_FFT = []
+Fy22_FFT = []
+
+Fz00_FFT = []
+Fz01_FFT = []
+Fz02_FFT = []
+Fz11_FFT = []
+Fz12_FFT = []
+Fz22_FFT = []
+
+
+def get_kmid(fft):
+    if fft.kx is not None:
+        kmid = fft.kx[np.where(fft.kx>=0)]
+    if fft.ky is not None:
+        kmid = fft.ky[np.where(fft.ky>=0)]
+    if fft.kz is not None:
+        kmid = fft.kz[np.where(fft.kz>=0)]
+    return kmid
+    
+def fft_power(fft):
+    # add another point to the end of the k grid for interpolation
+    # MAKES POWER SPECTRUM HAVE SIZE ONE LARGER THAN KTEMPLATE
+    kmid = get_kmid(fft)
+    dk = kmid[1]-kmid[0]
+    kmid = np.append(kmid, kmid[-1]+dk)
+    
+    # compute the magnitude of the wavevector for every point
+    kmag = 0
+    if fft.kx is not None:
+        kmag = kmag + fft.kx[:,np.newaxis,np.newaxis]**2
+    if fft.ky is not None:
+        kmag = kmag + fft.ky[np.newaxis,:,np.newaxis]**2
+    if fft.kz is not None:
+        kmag = kmag + fft.kz[np.newaxis,np.newaxis,:]**2
+    kmag = np.sqrt(np.squeeze(kmag))
+    kmag[np.where(kmag>=kmid[-1])] = kmid[-1]
+    
+ 
+    # compute left index for interpolation
+    ileft = (kmag/dk).astype(int)
+    iright = ileft+1
+    iright[np.where(iright>=len(kmid)-1)] = len(kmid)-1
+
+    # compute the fraction of the power that goes toward the left and right k point
+    cleft = (kmid[iright]-kmag)/dk
+    cright = 1.0-cleft
+
+    # compute power contributions to left and right indices
+    power = fft.magnitude**2
+    powerLeft = power*cleft
+    powerRight = power*cright
+
+    # accumulate onto spectrum
+    spectrum = np.zeros(len(kmid))
+    for i in range(len(kmid)):
+        spectrum[i] += np.sum( powerLeft[np.where( ileft==i)])
+        spectrum[i] += np.sum(powerRight[np.where(iright==i)])
+
+    return spectrum
 
 def get_matrix(base,suffix):
     f00  = ad['boxlib',base+"00_Re"+suffix]
@@ -91,9 +174,9 @@ t=[]
 
 for d in directories:
     print(d)
-    ds = yt.load(d)
-    t.append(ds.current_time)
-    ad = ds.all_data()
+    eds = emu.EmuDataset(d)
+    t.append(eds.ds.current_time)
+    ad = eds.ds.all_data()
 
     thisN, thisNI = get_matrix("N",""   )
     sumtrace = sumtrace_N(thisN)
@@ -118,7 +201,61 @@ for d in directories:
     Ftmp  = np.array([thisFx , thisFy , thisFz ])
     FtmpI = np.array([thisFxI, thisFyI, thisFzI])
     Fbar.append(averaged_F(Ftmp, FtmpI,sumtrace))
+
+    # FFT stuff
+    fft = eds.fourier("N00_Re")
+    N00_FFT.append(fft_power(fft))    
+    fft = eds.fourier("N11_Re")
+    N11_FFT.append(fft_power(fft))
+    fft = eds.fourier("N22_Re")
+    N22_FFT.append(fft_power(fft))
+    fft = eds.fourier("N01_Re","N01_Im")
+    N01_FFT.append(fft_power(fft))
+    fft = eds.fourier("N02_Re","N02_Im")
+    N02_FFT.append(fft_power(fft))
+    fft = eds.fourier("N12_Re","N12_Im")
+    N12_FFT.append(fft_power(fft))
     
+    fft = eds.fourier("Fx00_Re")
+    Fx00_FFT.append(fft_power(fft))
+    fft = eds.fourier("Fx11_Re")
+    Fx11_FFT.append(fft_power(fft))
+    fft = eds.fourier("Fx22_Re")
+    Fx22_FFT.append(fft_power(fft))
+    fft = eds.fourier("Fx01_Re","Fx01_Im")
+    Fx01_FFT.append(fft_power(fft))
+    fft = eds.fourier("Fx02_Re","Fx02_Im")
+    Fx02_FFT.append(fft_power(fft))
+    fft = eds.fourier("Fx12_Re","Fx12_Im")
+    Fx12_FFT.append(fft_power(fft))
+    
+    fft = eds.fourier("Fy00_Re")
+    Fy00_FFT.append(fft_power(fft))
+    fft = eds.fourier("Fy11_Re")
+    Fy11_FFT.append(fft_power(fft))
+    fft = eds.fourier("Fy22_Re")
+    Fy22_FFT.append(fft_power(fft))
+    fft = eds.fourier("Fy01_Re","Fy01_Im")
+    Fy01_FFT.append(fft_power(fft))
+    fft = eds.fourier("Fy02_Re","Fy02_Im")
+    Fy02_FFT.append(fft_power(fft))
+    fft = eds.fourier("Fy12_Re","Fy12_Im")
+    Fy12_FFT.append(fft_power(fft))
+    
+    fft = eds.fourier("Fz00_Re")
+    Fz00_FFT.append(fft_power(fft))
+    fft = eds.fourier("Fz11_Re")
+    Fz11_FFT.append(fft_power(fft))
+    fft = eds.fourier("Fz22_Re")
+    Fz22_FFT.append(fft_power(fft))
+    fft = eds.fourier("Fz01_Re","Fz01_Im")
+    Fz01_FFT.append(fft_power(fft))
+    fft = eds.fourier("Fz02_Re","Fz02_Im")
+    Fz02_FFT.append(fft_power(fft))
+    fft = eds.fourier("Fz12_Re","Fz12_Im")
+    Fz12_FFT.append(fft_power(fft))
+
+    kmid = get_kmid(fft)
 
 trace = np.array(trace)
 N = np.array(N)
@@ -136,3 +273,32 @@ avgData["Fbar_avg_mag"] = Fbar
 avgData["t"] = t
 avgData.close()
 
+# write FFT file
+f = h5py.File("reduced_data_fft_power.h5","w")
+f["t"] = np.array(t)
+f["k"] = kmid
+f["N00_FFT"] = np.array(N00_FFT)
+f["N11_FFT"] = np.array(N11_FFT)
+f["N22_FFT"] = np.array(N22_FFT)
+f["N01_FFT"] = np.array(N01_FFT)
+f["N02_FFT"] = np.array(N02_FFT)
+f["N12_FFT"] = np.array(N12_FFT)
+f["Fx00_FFT"] = np.array(Fx00_FFT)
+f["Fx11_FFT"] = np.array(Fx11_FFT)
+f["Fx22_FFT"] = np.array(Fx22_FFT)
+f["Fx01_FFT"] = np.array(Fx01_FFT)
+f["Fx02_FFT"] = np.array(Fx02_FFT)
+f["Fx12_FFT"] = np.array(Fx12_FFT)
+f["Fy00_FFT"] = np.array(Fy00_FFT)
+f["Fy11_FFT"] = np.array(Fy11_FFT)
+f["Fy22_FFT"] = np.array(Fy22_FFT)
+f["Fy01_FFT"] = np.array(Fy01_FFT)
+f["Fy02_FFT"] = np.array(Fy02_FFT)
+f["Fy12_FFT"] = np.array(Fy12_FFT)
+f["Fz00_FFT"] = np.array(Fz00_FFT)
+f["Fz11_FFT"] = np.array(Fz11_FFT)
+f["Fz22_FFT"] = np.array(Fz22_FFT)
+f["Fz01_FFT"] = np.array(Fz01_FFT)
+f["Fz02_FFT"] = np.array(Fz02_FFT)
+f["Fz12_FFT"] = np.array(Fz12_FFT)
+f.close()
