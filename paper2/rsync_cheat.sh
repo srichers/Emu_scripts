3e166dfaee8c6cf21f5882bbd2b4cb62647169bf
Sherwood Richers <srichers@caltech.edu>
2021-06-22 16:28:03 -0700

    make sure we dont exclude directories with a period in the name

--- paper2/rsync_cheat.sh
+++ paper2/rsync_cheat.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-rsync --exclude="Emu/" --exclude="*plt*" --include="*.h5" --exclude="*.*" --relative --recursive --verbose --dry-run --prune-empty-dirs srichers@cori.nersc.gov:/global/project/projectdirs/m3761 .
+rsync --exclude="Emu/" --exclude="*plt*" --include="*.h5" --include="*/" --exclude="*.*" --relative --recursive --verbose --prune-empty-dirs srichers@cori.nersc.gov:/global/project/projectdirs/m3761 .
 
-rsync --exclude="*plt*" --include="*.h5" --exclude="*.*" --relative --recursive --verbose --dry-run --prune-empty-dirs srichers@cori.nersc.gov:/global/project/projectdirs/m3018/Emu .
+rsync --exclude="*plt*" --include="*.h5" --include="*/" --exclude="*.*" --relative --recursive --verbose --prune-empty-dirs srichers@cori.nersc.gov:/global/project/projectdirs/m3018/Emu .
 
-rsync --exclude="*plt*" --include="*.h5" --exclude="*.*" --relative --recursive --verbose --dry-run --prune-empty-dirs saricher@bridges2.psc.edu:/ocean/projects/phy200048p/shared .
+rsync --exclude="*plt*" --include="*.h5" --include="*/" --exclude="*.*" --relative --recursive --verbose --prune-empty-dirs saricher@bridges2.psc.edu:/ocean/projects/phy200048p/shared .
+
+# note: using -vv will tell you which rule included/excluded each file/folder
 
 #rsync -vaz srichers@cori.nersc.gov:/global/project/projectdirs/m3761/*/**/*.h5 . --relative
 #rsync -vaz srichers@cori.nersc.gov:/global/project/projectdirs/m3018/Emu/*/**/*.h5 . --relative
