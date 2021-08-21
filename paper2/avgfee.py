155e5e0095b687c7ac42979b7055e5a950bafc92
Sherwood Richers <srichers@berkeley.edu>
2021-08-21 13:49:06 -0700

    use latex fonts and update plots with current dataset locations on cori

--- paper2/avgfee.py
+++ paper2/avgfee.py
@@ -38,7 +38,7 @@ def plotdata(filename):
 ################
 mpl.rcParams['font.size'] = 22
 mpl.rcParams['font.family'] = 'serif'
-#mpl.rc('text', usetex=True)
+mpl.rc('text', usetex=True)
 mpl.rcParams['xtick.major.size'] = 7
 mpl.rcParams['xtick.major.width'] = 2
 mpl.rcParams['xtick.major.pad'] = 8
@@ -58,9 +58,8 @@ plt.subplots_adjust(hspace=0,wspace=0)
 # formatting #
 ##############
 for ax in axes:
-    ax.axhline(1./3., color="green")
-    ax.set_ylabel(r"$\langle N_{ee}\rangle /\mathrm{Tr}(N)$")
-    ax.set_xlim(.1,5)
+    ax.set_ylabel(r"$\langle \rho_{ee}\rangle$")
+    ax.set_xlim(0,5)
     #ax.set_xscale("log")
     ax.tick_params(axis='both', which='both', direction='in', right=True,top=True)
     ax.xaxis.set_minor_locator(AutoMinorLocator())
@@ -69,9 +68,11 @@ for ax in axes:
 #############
 # plot data #
 #############
-dirlist    = [["-", "gray" , "1D", "ocean/projects/phy200048p/shared/1D/1D_fiducial"],
-              ["-", "black", "2D", "ocean/projects/phy200048p/shared/2D/fiducial_2D"],
-              ["-", "blue" , "3D", "global/project/projectdirs/m3761/3D/128r64d_128n800s16mpi"]]
+basedir = "/global/project/projectdirs/m3761/PAPER2"
+dirlist    = [["-", "gray" , "1D", basedir+"/Fiducial_1D"],
+              ["-", "black" , "2D", basedir+"/Fiducial_2D"],
+              ["-", "blue" , "3D", basedir+"/Fiducial_3D"],]
+axes[0].axhline(1./3., color="green")
 for inputs in dirlist:
     filename = inputs[3]+"/reduced_data.h5"
     t,N = plotdata(filename)
@@ -81,31 +82,35 @@ for inputs in dirlist:
     axes[0].plot(t, N,color=inputs[1],linestyle=inputs[0],label=inputs[2])
 
 
-dirlist    = [["-" , "gray" , "1D"               , "ocean/projects/phy200048p/shared/1D/90deg"],
-              ["-" , "black", "2D (in plane)"    , "ocean/projects/phy200048p/shared/2D/90deg_inplane"],
-              ["--", "black", "2D (out of plane)", "ocean/projects/phy200048p/shared/2D/90deg_outofplane"],
-              ["-" , "blue" , "3D"               , "global/project/projectdirs/m3761/3D/90degree_64d"]]
+dirlist    = [["-" , "gray" , "1D"               , basedir+"/90Degree_1D"],
+              #["-" , "black", "2D (in plane)"    , "ocean/projects/phy200048p/shared/2D/90deg_inplane"],
+              ["-", "black", "2D", basedir+"/90Degree_2D_outplane"],
+              ["-" , "blue" , "3D"               , basedir+"/90Degree_3D"]]
+axes[1].axhline(1./3., color="green")
 for inputs in dirlist:
     filename = inputs[3]+"/reduced_data.h5"
     t,N = plotdata(filename)
     axes[1].plot(t, N,color=inputs[1],linestyle=inputs[0],label=inputs[2])
 
-dirlist    = [["-", "gray", "1D", "global/project/projectdirs/m3018/Emu/PAPER/1D/rando_test/1.0_thirds"],
-              ["-", "blue", "3D", "global/project/projectdirs/m3761/3D/two_thirds"]]
+dirlist    = [["-", "gray", "1D", basedir+"/TwoThirds_1D"],
+              ["-", "black", "2D", basedir+"/TwoThirds_2D"],
+              ["-", "blue", "3D", basedir+"/TwoThirds_3D/1"]]
+axes[2].axhline(0.7, color="green")
 for inputs in dirlist:
     filename = inputs[3]+"/reduced_data.h5"
     t,N = plotdata(filename)
     axes[2].plot(t, N,color=inputs[1],linestyle=inputs[0],label=inputs[2])
 
 
-axes[0].legend(frameon=False,ncol=1,fontsize=18, loc=(.2,.3))
-axes[1].legend(frameon=False,ncol=1,fontsize=18, loc=(.2,.3))
-axes[2].legend(frameon=False,ncol=1,fontsize=18, loc=(.2,.3))
+axes[0].legend(frameon=False,ncol=1,fontsize=18, loc=(.4,.3))
+#axes[1].legend(frameon=False,ncol=1,fontsize=18, loc=(.4,.3))
+#axes[2].legend(frameon=False,ncol=1,fontsize=18, loc=(.4,.3))
 axes[0].set_xticklabels([])
 axes[1].set_xticklabels([])
 axes[0].text(2.5,.9,"Fiducial")
-axes[1].text(2.5,.9,"90 Degree")
-axes[2].text(2.5,.9,"2/3")
+axes[1].text(2.5,.9,"90Degree")
+axes[2].text(2.5,.9,"TwoThirds")
+axes[2].set_ylim(.6,1.05)
 axes[2].set_xlabel(r"$t\,(10^{-9}\,\mathrm{s})$")
 
 
