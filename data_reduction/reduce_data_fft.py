37376f6be2f74bbbfe9ca56011e8cc5d512b7a56
Sherwood Richers <srichers@berkeley.edu>
2022-02-16 13:38:11 -0800

    make the fft script work on either 2- or 3-flavor output

--- data_reduction/reduce_data_fft.py
+++ data_reduction/reduce_data_fft.py
@@ -47,6 +47,7 @@ Fx22_FFT_phase = []
 for d in directories:
     print(d)
     eds = emu.EmuDataset(d)
+    NF = eds.get_num_flavors()
     t.append(eds.ds.current_time)
 
     fft = eds.fourier("N00_Re")
@@ -57,21 +58,9 @@ for d in directories:
     N11_FFT.append(fft.magnitude)
     N11_FFT_phase.append(fft.phase)
 
-    fft = eds.fourier("N22_Re")
-    N22_FFT.append(fft.magnitude)
-    N22_FFT_phase.append(fft.phase)
-    
     fft = eds.fourier("N01_Re","N01_Im")
     N01_FFT.append(fft.magnitude)
     N01_FFT_phase.append(fft.phase)
-    
-    fft = eds.fourier("N02_Re","N02_Im")
-    N02_FFT.append(fft.magnitude)
-    N02_FFT_phase.append(fft.phase)
-    
-    fft = eds.fourier("N12_Re","N12_Im")
-    N12_FFT.append(fft.magnitude)
-    N12_FFT_phase.append(fft.phase)
     
     fft = eds.fourier("Fx00_Re")
     Fx00_FFT.append(fft.magnitude)
@@ -81,22 +70,36 @@ for d in directories:
     Fx11_FFT.append(fft.magnitude)
     Fx11_FFT_phase.append(fft.phase)
 
-    fft = eds.fourier("Fx22_Re")
-    Fx22_FFT.append(fft.magnitude)
-    Fx22_FFT_phase.append(fft.phase)
-    
     fft = eds.fourier("Fx01_Re","Fx01_Im")
     Fx01_FFT.append(fft.magnitude)
     Fx01_FFT_phase.append(fft.phase)
     
-    fft = eds.fourier("Fx02_Re","Fx02_Im")
-    Fx02_FFT.append(fft.magnitude)
-    Fx02_FFT_phase.append(fft.phase)
+    if NF>2:
+        fft = eds.fourier("N22_Re")
+        N22_FFT.append(fft.magnitude)
+        N22_FFT_phase.append(fft.phase)
     
-    fft = eds.fourier("Fx12_Re","Fx12_Im")
-    Fx12_FFT.append(fft.magnitude)
-    Fx12_FFT_phase.append(fft.phase)
-
+        fft = eds.fourier("N02_Re","N02_Im")
+        N02_FFT.append(fft.magnitude)
+        N02_FFT_phase.append(fft.phase)
+        
+        fft = eds.fourier("N12_Re","N12_Im")
+        N12_FFT.append(fft.magnitude)
+        N12_FFT_phase.append(fft.phase)
+        
+        fft = eds.fourier("Fx22_Re")
+        Fx22_FFT.append(fft.magnitude)
+        Fx22_FFT_phase.append(fft.phase)
+        
+        fft = eds.fourier("Fx02_Re","Fx02_Im")
+        Fx02_FFT.append(fft.magnitude)
+        Fx02_FFT_phase.append(fft.phase)
+        
+        fft = eds.fourier("Fx12_Re","Fx12_Im")
+        Fx12_FFT.append(fft.magnitude)
+        Fx12_FFT_phase.append(fft.phase)
+        
+        
 ##################
 # write the file #
 ##################
@@ -113,28 +116,32 @@ if fft.kz is not None:
 
 f["N00_FFT"] = np.array(N00_FFT)
 f["N11_FFT"] = np.array(N11_FFT)
-f["N22_FFT"] = np.array(N22_FFT)
 f["N01_FFT"] = np.array(N01_FFT)
-f["N02_FFT"] = np.array(N02_FFT)
-f["N12_FFT"] = np.array(N12_FFT)
 f["Fx00_FFT"] = np.array(Fx00_FFT)
 f["Fx11_FFT"] = np.array(Fx11_FFT)
-f["Fx22_FFT"] = np.array(Fx22_FFT)
 f["Fx01_FFT"] = np.array(Fx01_FFT)
-f["Fx02_FFT"] = np.array(Fx02_FFT)
-f["Fx12_FFT"] = np.array(Fx12_FFT)
 
 f["N00_FFT_phase"] = np.array(N00_FFT_phase)
 f["N11_FFT_phase"] = np.array(N11_FFT_phase)
-f["N22_FFT_phase"] = np.array(N22_FFT_phase)
 f["N01_FFT_phase"] = np.array(N01_FFT_phase)
-f["N02_FFT_phase"] = np.array(N02_FFT_phase)
-f["N12_FFT_phase"] = np.array(N12_FFT_phase)
 f["Fx00_FFT_phase"] = np.array(Fx00_FFT_phase)
 f["Fx11_FFT_phase"] = np.array(Fx11_FFT_phase)
-f["Fx22_FFT_phase"] = np.array(Fx22_FFT_phase)
 f["Fx01_FFT_phase"] = np.array(Fx01_FFT_phase)
-f["Fx02_FFT_phase"] = np.array(Fx02_FFT_phase)
-f["Fx12_FFT_phase"] = np.array(Fx12_FFT_phase)
+
+if NF>2:
+    f["N22_FFT"] = np.array(N22_FFT)
+    f["N02_FFT"] = np.array(N02_FFT)
+    f["N12_FFT"] = np.array(N12_FFT)
+    f["Fx22_FFT"] = np.array(Fx22_FFT)
+    f["Fx02_FFT"] = np.array(Fx02_FFT)
+    f["Fx12_FFT"] = np.array(Fx12_FFT)
+
+    f["N22_FFT_phase"] = np.array(N22_FFT_phase)
+    f["N02_FFT_phase"] = np.array(N02_FFT_phase)
+    f["N12_FFT_phase"] = np.array(N12_FFT_phase)
+    f["Fx22_FFT_phase"] = np.array(Fx22_FFT_phase)
+    f["Fx02_FFT_phase"] = np.array(Fx02_FFT_phase)
+    f["Fx12_FFT_phase"] = np.array(Fx12_FFT_phase)
+
 
 f.close()
