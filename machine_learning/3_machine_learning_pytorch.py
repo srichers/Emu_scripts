0ce2a44f63234f3e8f5ecb7753a48f05ec67fc1c
Sherwood Richers <srichers@caltech.edu>
2022-10-14 16:31:21 -0700

    training to return deltas instead of actual values works! Preserves traces.

--- machine_learning/3_machine_learning_pytorch.py
+++ machine_learning/3_machine_learning_pytorch.py
@@ -14,15 +14,13 @@ from itertools import permutations
 input_filename = "many_sims_database_RUN_lowres_sqrt2_RUN_standard.h5"
 N_Nbar_tolerance = 1e-3
 NF=2
-epochs = 20
+epochs = 5
 
 #===============================================#
 # read in the database from the previous script #
 #===============================================#
 f_in = h5py.File(input_filename,"r")
-#ijkList_growthrate = np.array(f_in["ijk_growthrate"])
 growthRateList     = np.array(f_in["growthrate(1|s)"])
-#ijkList_F4         = np.array(f_in["ijk_F4"])
 F4_initial_list    = np.array(f_in["F4_initial_Nsum1"]) # [ind, xyzt, nu/nubar, flavor]
 F4_final_list      = np.array(f_in["F4_final_Nsum1"])
 f_in.close()
@@ -43,20 +41,10 @@ y = F4_final_list
 
 # split the data into training and testing data
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-print("X_train:", X_train.shape)
-print("y_train:", y_train.shape)
-print("X_test:", X_test.shape)
-print("y_test:", y_test.shape)
+print("split train shape = ", X_train.shape, y_train.shape)
+print("split test shape = ",X_test.shape, y_test.shape)
 n_train = X_train.shape[0]
 n_test = X_test.shape[0]
-
-# create test ("Fiducial" simulation)
-F4_test = np.zeros(IO_shape)
-F4_test[3,0,0] = 1
-F4_test[3,1,0] = 1
-F4_test[2,0,0] = 1/3
-F4_test[2,1,0] = -1/3
-F4_test /= np.sum(F4_test[3])
 
 # use a GPU if available
 device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -65,34 +53,6 @@ X_train = torch.Tensor(X_train).to(device)
 y_train = torch.Tensor(y_train).to(device)
 X_test = torch.Tensor(X_test).to(device)
 y_test = torch.Tensor(y_test).to(device)
-
-# define the NN model
-class NeuralNetwork(nn.Module):
-    def __init__(self):
-        super().__init__()
-        #self.flatten = nn.Flatten()
-        self.linear_relu_stack = nn.Sequential(
-            nn.Linear(16, 32),
-            nn.ReLU(),
-            nn.Linear(32, 32),
-            nn.ReLU(),
-            nn.Linear(32,16)
-        )
-
-    def forward(self,x):
-        #x = self.flatten(x)
-        logits = self.linear_relu_stack(x)
-        return logits
-
-# instantiate the model
-model = NeuralNetwork().to(device)
-print(model)
-
-# define the loss function
-loss_fn = nn.MSELoss(reduction='sum')
-
-# use the adam optimizer
-optimizer = torch.optim.Adam(model.parameters())
 
 # create list of equivalent simulations
 # X = [isim, xyzt, nu/nubar, flavor]
@@ -137,14 +97,21 @@ def augment_data(X,y):
                             yaugmented[iaug,:,2] *= reflect2
                             
     # flatten the input/output. Torch expects the last dimension size to be the number of features.
-    Xaugmented = torch.flatten(Xaugmented,start_dim=2)
-    yaugmented = torch.flatten(yaugmented,start_dim=2)
+    #Xaugmented = torch.flatten(Xaugmented,start_dim=2)
+    #yaugmented = torch.flatten(yaugmented,start_dim=2)
 
     # switch the array index order to the simulation index is first and the augmentation index is second
     Xaugmented = torch.transpose(Xaugmented,0,1)
     yaugmented = torch.transpose(yaugmented,0,1)
 
     return Xaugmented, yaugmented
+
+# augment both datasets
+X_train, y_train = augment_data(X_train, y_train)
+X_test,  y_test  = augment_data(X_test,  y_test )
+print("augmented training data shape:",X_train.shape, y_train.shape)
+print("augmented testing data shape:",X_test.shape, y_test.shape)
+
 
 # function to train the dataset
 def train_chunked(Xlist,ylist, model, loss_fn, optimizer):
@@ -195,12 +162,94 @@ def test(Xlist,ylist, model, loss_fn):
         test_loss = loss_fn(pred,ylist).item()
         print(f"Test Error: {test_loss:>8f}")
 
-# augment both datasets
-X_train, y_train = augment_data(X_train, y_train)
-X_test,  y_test  = augment_data(X_test,  y_test )
-print("augmented training data shape:",X_train.shape)
-print("augmented testing data shape:",X_test.shape)
+def cyclic_permute(A, steps):
+    ndims = len(A.shape)
+    if(steps>0): steps -= ndims
+    permutation = [range(ndims)[i + steps] for i in range(ndims)]
+    return torch.permute(A,permutation)
+    
+# generate y from final distributions
+# y quantifies the amount of flow from flavor i to flavor i+1
+# We do not consider antineutrino number density flux, because it must be identical to that for neutrinos
+# For NF flavors, there are NF-1 flow quantities for each of 7 components
+# assumes final three indices index into distribution properties and other indices identify samples
+Ny = (NF-1)*7
+def y_from_F4(F4_initial, F4_final):
+    # take the difference between the final and initial solutions
+    delta_F4 = F4_final - F4_initial # [isim1, isim2,..., xyzt, nu/nubar, flavor]
 
+    # permute the indices so the sample indices are at the end and we can use the same expressions for one data point and many data points
+    input_rank = len(delta_F4.shape)
+    single_element_rank = 3
+    assert(input_rank >= single_element_rank)
+    shift = input_rank - single_element_rank
+    delta_F4 = cyclic_permute(delta_F4, shift) # [xyzt, nu/nubar, flavor, isim1, isim2, ...]
+
+    # calculate the flow from one flavor to the next
+    delta_F4_flow = delta_F4[:,:,1:NF] - delta_F4[:,:,0:NF-1]
+
+    # create y array of correct size
+    # [iy, isim1, isim2, ...]
+    shape = [Ny]
+    if input_rank>single_element_rank:
+        for dim in range(3,input_rank):
+            shape.append(delta_F4.shape[dim])
+    y = torch.zeros(shape).to(device)
+
+    # fill in y
+    diy = (NF-1)*2
+    for i in range(3):
+        iystart = diy*i
+        y[iystart:iystart+diy] = delta_F4_flow[i].flatten(start_dim=0, end_dim=1)
+    iystart = diy*3
+    y[iystart:iystart+(NF-1)] = delta_F4_flow[3,0]
+
+    # shift back to have simulation index first
+    y = cyclic_permute(y,-shift) # [isim1, isim1, ... , iy]
+    return y
+
+#def F4_from_y(F4_initial, y):
+#    F4_final = copy.deepcopy(F4_initial)
+
+#    F4_final[]
+
+y_train = y_from_F4(X_train, y_train)
+y_test = y_from_F4(X_test, y_test)
+X_train = torch.flatten(X_train,start_dim=2)
+X_test = torch.flatten(X_test,start_dim=2)
+
+print("Restructured train shape:",X_train.shape, y_train.shape)
+print("Restructured test shape:",X_test.shape, y_test.shape)
+
+# define the NN model
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        #self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(16, 64),
+            nn.ReLU(),
+            nn.Linear(64, 64),
+            nn.ReLU(),
+            nn.Linear(64, 64),
+            nn.ReLU(),
+            nn.Linear(64,Ny)
+        )
+
+    def forward(self,x):
+        #x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+
+# instantiate the model
+model = NeuralNetwork().to(device)
+print(model)
+
+# define the loss function
+loss_fn = nn.MSELoss(reduction='sum')
+
+# use the adam optimizer
+optimizer = torch.optim.Adam(model.parameters())
 
 # training loop
 for t in range(epochs):
@@ -210,32 +259,37 @@ for t in range(epochs):
     train(X_train, y_train, model, loss_fn, optimizer)
     test(X_test, y_test, model, loss_fn)
 print("Done!")
-exit()
 
-#===============================#
-# Abstract out boilerplate code #
-#===============================#
-def run_ML_model(estimator, param_grid, label):
+#=====================================#
+# create test ("Fiducial" simulation) #
+#=====================================#
+F4_test = np.zeros(IO_shape)
+F4_test[3,0,0] = 1
+F4_test[3,1,0] = 1
+F4_test[2,0,0] = 1/3
+F4_test[2,1,0] = -1/3
+F4_test /= np.sum(F4_test[3])
 
-    # run the grid search
-    StartTime = time.time()
-    #grid_search.fit(X,y)
-    EndTime = time.time()
+before = torch.Tensor(F4_test).to(device)
+shape = F4_test.shape
+print("before:",before.shape)
+after = model(before.flatten(start_dim=0)).reshape(shape)
+print("after:",after.shape)
 
-    print("Total time:",EndTime-StartTime,"seconds.")
 
-    before = F4_test
-    after = F4_test #grid_search.predict(F4_test.reshape((1,number_predictors))).reshape(IO_shape)
-    print("N Before:", before[3].flatten(), np.sum(before[3,0]), np.sum(before[3,1]), np.sum(before[3]))
-    print("N After :", after[3].flatten(), np.sum(after[3,0]), np.sum(after[3,1]), np.sum(after[3]))
-    print()
-    print("Fz Before:", before[2].flatten(), np.sum(before[2]))
-    print("Fz After :", after[2].flatten(), np.sum(after[2]))
-    print()
-    print("Fy Before:", before[1].flatten(), np.sum(before[1]))
-    print("Fy After :", after[1].flatten(), np.sum(after[1]))
-    print()
-    print("Fx Before:", before[0].flatten(), np.sum(before[0]))
-    print("Fx After :", after[0].flatten(), np.sum(after[0]))
-    print()
+before = before.to('cpu').detach().numpy()
+after = after.to('cpu').detach().numpy()
+
+print("N Before:", before[3].flatten(), np.sum(before[3]))
+print("N After :", after[3].flatten() , np.sum(after[3]) )
+print()
+print("Fz Before:", before[2].flatten(), np.sum(before[2]))
+print("Fz After :", after[2].flatten() , np.sum(after[2]) )
+print()
+print("Fy Before:", before[1].flatten(), np.sum(before[1]))
+print("Fy After :", after[1].flatten() , np.sum(after[1]) )
+print()
+print("Fx Before:", before[0].flatten(), np.sum(before[0]))
+print("Fx After :", after[0].flatten() , np.sum(after[0]) )
+print()
 
