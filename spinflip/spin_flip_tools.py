0e311364b167b2251a807454d403e9eee6426ab3
Sherwood Richers <richers@utk.edu>
2023-04-22 14:49:04 -0400

    move matrix operations to a separate file

--- spinflip/spin_flip_tools.py
+++ spinflip/spin_flip_tools.py
@@ -22,6 +22,7 @@ from multiprocessing import Pool
 from constants import a12, a23, a13, delta, m_1, m_2, m_3, p_abs, M_p, hbar, c, G
 from diagonalizer import Diagonalizer
 from basis import Basis
+from matrix import visualizer, dagger, trace_matrix
 
 
 #takes in value that depends on theta, phi and returns a theta_res by phi_res array of values 
@@ -29,57 +30,6 @@ def angularArray(func, theta_res, phi_res):
     return np.array([[func(theta, phi) for phi in np.linspace(0, 2*np.pi, phi_res)]
                                       for theta in np.linspace(0, np.pi, theta_res)])
 
-def rm_trace(M):
-    return np.array(M) - np.trace(M)*np.identity(np.array(M).shape[0])/np.array(M).shape[0]
-
-def visualizer(M, log=True, text='mag', traceless = False, vmin=1E-15,vmax=1E-6, savefig=False):
-    if traceless ==True:
-        M=rm_trace(M)
-    else:
-        M=np.array(M)
-    
-    matrix_size = M.shape[0]
-    grid_subdivisions = 1/matrix_size
-    vertices_x = [n*grid_subdivisions for n in np.arange(0,matrix_size+1)]
-    vertices_y = [n*grid_subdivisions for n in np.arange(matrix_size,-1,-1)]
-    
-    min_value=vmin
-    max_value=vmax
-    
-    if log ==True: scale = norm=colors.LogNorm(vmin=min_value, vmax=max_value)
-    else: scale = None
-    
-    
-    f, ax = plt.subplots()
-    ax.set_ylim(0,1)
-    ax.set_xlim(0,1)
-    
-    image = ax.pcolormesh(vertices_x,vertices_y,np.abs(M),cmap='hot',norm=scale)
-    plt.colorbar(image)
-    
-    if text=='mag':
-        for n in np.arange(0,matrix_size):
-            for m in np.arange(0,matrix_size):
-                xcoord = (n+1/6)*grid_subdivisions
-                ycoord = 1 - (m+1/2)*grid_subdivisions
-                ax.text(xcoord, ycoord, str(round(np.log10(np.abs(M[m,n])),0)), color='cyan', size=10)
-                
-    elif text=='arg':
-        for n in np.arange(0,matrix_size):
-            for m in np.arange(0,matrix_size):
-                xcoord = (n)*grid_subdivisions
-                ycoord = 1 - (m+1/2)*grid_subdivisions
-                ax.text(xcoord, ycoord, str(round(np.real(M[m,n]/np.abs(M[m,n])), ndigits=1))+'+'+str(round(np.imag(M[m,n])/np.abs(M[m,n]), ndigits=2))+'i', color='cyan', size=9)
-
-    if savefig == True: 
-        plt.tight_layout()
-        plt.savefig('visualizer.png', dpi=300)
-
-                
-    
-        
-        
-        
         
 #Given a spinflip dataset, finds the hamiltonian at some angle. Can also check resonant direction
 #t_sim is ffi simulation timestep to calculate parameters at
@@ -205,7 +155,7 @@ class SpinParams:
         return np.real(2**(-1/2)*G*self.n_b*(3.*self.Ye-np.ones_like(self.Ye))+self.S_R_kappa(theta,phi)[0,0])
 
     def angularPlot(self, theta_res, phi_res, savefig=False, use_gm=True, direction_point=False):
-            
+        
         if use_gm==True:
             H_LR_array = np.array([[np.abs(np.trace(self.H_LR(theta, phi))) 
                                    for phi in np.linspace(0, 2*np.pi, phi_res)]
@@ -653,25 +603,6 @@ class Multipoint:
         
         
         
-#unitary trace matrix
-def trace_matrix(data):#takes in (nF,nF,nz)
-        nz = data.shape[2]
-        nF = data.shape[0]
-        # calculate trace
-        diagonals = np.array([data[n,n,:] for n in range(nF)])
-        trace = np.sum(diagonals, axis=0) # length of nz
-        
-        # create an identity matrix multiplied by the trace
-        matrix=np.zeros_like(data)
-        for n in range(nF):
-                matrix[n,n,:]=trace
-        
-        return matrix
-	
-#conjugate a matrix
-def dagger(matrix):
-	conjugate=np.transpose(np.conjugate(matrix))
-	return conjugate
 	
 #z-derivative of (nF,nF,nz) matrix (returns nF,nF,nz)
 #TODO - needs to implement periodic boundary conditions.
