251988bad1a230b884e2e67664caff5eb3a1b79f
Sherwood Richers <richers@utk.edu>
2023-04-22 15:20:32 -0400

    move SpinParams to its own file

--- spinflip/constants.py
+++ spinflip/constants.py
@@ -1,4 +1,5 @@
 import numpy as np
+from matrix import dagger
 
 #fermi coupling constant: G/(c*hbar)^3=1.166 378 7(6)×10−5 GeV−2 --> G=1.166 378 7×10−23 eV^−2 (natural units:c=hbar=1)
 G=1.1663787*10**(-23) # eV^-2
@@ -22,3 +23,26 @@ m_3=0.649487372
 
 #test neutrino momentum:
 p_abs=10**7#eV
+
+## Mass Matrix ##	
+m23=np.array([[1,0*1j,0],
+              [0,np.cos(a23),np.sin(a23)],
+              [0,-np.sin(a23),np.cos(a23)]])
+
+m13=np.array([[np.cos(a13),0,np.sin(a13)*np.exp(-1j*delta)],
+              [0,1,0],
+              [-np.sin(a13)*np.exp(1j*delta),0,np.cos(a13)]])
+
+m12=np.array([[np.cos(a12),np.sin(a12),0],
+              [-np.sin(a12),np.cos(a12),0],
+              [0,0*1j,1]])
+
+m=m23 @ m13 @ m12
+#m is the mass mixing (MNS) matrix -- m*(1,2,3)=(e,mu,tau)
+M_mass_basis=([[m_1,0*1j,0],[0,m_2,0],[0,0,m_3]])
+M_3flavor = m @ M_mass_basis @ dagger(m) #(3,3)
+M=M_3flavor
+
+m2 = np.array([[np.cos(a12),np.sin(a12)],[-np.sin(a12),np.cos(a12)]])
+M_mass_basis_2 =([[m_1,0*1j],[0,m_2]])
+M_2flavor = m2 @ M_mass_basis_2 @ dagger(m2)
--- spinflip/spin_flip_tools.py
+++ spinflip/spin_flip_tools.py
@@ -7,234 +7,27 @@ import sys
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 sys.path.append(os.path.dirname(os.path.realpath(__file__))+"/../data_reduction")
 import numpy as np
+import emu_yt_module as emu
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
-from scipy import optimize as opt
 import h5py
 import gellmann as gm
 import glob
 import concurrent
 import matplotlib as mpl
-from matplotlib import cm, colors
 from multiprocessing import Pool
-from constants import a12, a23, a13, delta, m_1, m_2, m_3, p_abs, M_p, hbar, c, G
+from constants import p_abs, M_p, hbar, c, G, M_3flavor, M_2flavor
 from diagonalizer import Diagonalizer
 from basis import Basis
 from matrix import visualizer, dagger, trace_matrix
-from four_current import calculator_h5, calculator_eds
+from four_current import calculator_h5, calculator_eds, four_current_h5, four_current_eds
+from spin_params import SpinParams, sigma
 
 
 #takes in value that depends on theta, phi and returns a theta_res by phi_res array of values 
 def angularArray(func, theta_res, phi_res):   
     return np.array([[func(theta, phi) for phi in np.linspace(0, 2*np.pi, phi_res)]
                                       for theta in np.linspace(0, np.pi, theta_res)])
-
-        
-#Given a spinflip dataset, finds the hamiltonian at some angle. Can also check resonant direction
-#t_sim is ffi simulation timestep to calculate parameters at
-#data_loc is the spinflip file to compute
-#merger_data_loc is the merger grid data location
-#location is where in the merger data to evaluate stuff like ye, rho (=[x,y,z])
-
-class SpinParams:
-    def __init__(self, t_sim, data_loc, merger_data_loc, location,
-                 p_abs=10**7):
-        
-        self.data_loc = data_loc
-        self.h5file = h5py.File(self.data_loc, "r")
- 
-        self.p_abs = p_abs
-        self.t_sim = t_sim
-         
-        #Grid-dependent stuff: Electron fraction, baryon n density
-        self.location=location
-          
-        self.merger_grid = h5py.File(merger_data_loc, 'r')
-        self.rho = np.array(self.merger_grid['rho(g|ccm)'])[location[0],location[1],location[2]] #g/cm^3 (baryon mass density)
-        self.Ye = np.array(self.merger_grid['Ye'])[location[0],location[1],location[2]]
-        self.n_b = self.rho/M_p*(hbar**3 * c**3)#eV^3 (baryon number density)
-    
-            
-        #Flux (spacetime, F, F, z)
-        self.J = total(self.h5file, 'J(eV^3)')[self.t_sim]
-       
-        #length of 1d array 
-        self.nz = self.J.shape[3]
-        
-        #neutrino part of Sigma
-        self.S_R_nu = sigma(self.J)[0]
-        self.S_L_nu = sigma(self.J)[1]
-        
-        #matter part of Sigma
-        self.S_R_mat = np.zeros(np.shape(self.J))  
-        for k in np.arange(0, self.nz):
-            self.S_R_mat[0,:,:,k] = 2**(-1/2)*G*self.n_b*np.array([[3*self.Ye-1,    0,      0],
-                                              [0,           self.Ye-1, 0],
-                                              [0,              0,   self.Ye-1 ]])
-        self.S_L_mat = (-1)*np.transpose(self.S_R_mat, axes=(0,2,1,3))   
-        
-        #Total Sigma
-        self.S_R = self.S_R_nu + self.S_R_mat
-        self.S_L = self.S_L_nu + self.S_R_mat
-        
-        #Mass part
-        self.M = M_3flavor
-        self.H_vac = 1/(2*self.p_abs)*np.matmul(self.M,dagger(self.M))
-        
-        
-
-    def S_L_kappa(self, theta, phi):
-        basis = Basis(theta,phi)
-        return np.average(basis.kappa(self.S_L), axis = 2)
-
-    def S_R_kappa(self, theta, phi):
-        basis = Basis(theta,phi)
-        return np.average(basis.kappa(self.S_R), axis = 2)
-
-    def H_L_pm(self, theta, phi):
-        basis = Basis(theta,phi)
-        S_L_minus = basis.minus(self.S_L)
-        S_L_plus = basis.plus(self.S_L)
-        H_L_pm = 2./self.p_abs * np.array([np.matmul(S_L_minus[:,:,z], S_L_plus[:,:,z])
-            for z in range(self.nz)]).transpose((1,2,0))
-        return np.average(H_L_pm, axis=2)
-
-    def H_R_pm(self, theta, phi):
-        basis = Basis(theta,phi)
-        S_R_minus = basis.minus(self.S_R)
-        S_R_plus = basis.plus(self.S_R)
-        H_R_pm = 2./self.p_abs * np.array([np.matmul(S_R_plus[:,:,z], S_R_minus[:,:,z])
-            for z in range(self.nz)]).transpose((1,2,0))
-        return  np.average(H_R_pm, axis = 2)
-
-
-    #NO DERIVATIVE TERM
-    def H_L(self, theta, phi):
-        basis = Basis(theta,phi)
-        return self.S_L_kappa(theta, phi) + self.H_vac + self.H_L_pm(theta, phi)
-
-    def H_R(self, theta, phi):
-        basis = Basis(theta,phi)
-        return self.S_R_kappa(theta, phi) + self.H_vac + self.H_R_pm(theta, phi)
-
-    def H_LR(self, theta, phi):          
-        basis = Basis(theta, phi)
-        S_L_plus = np.average(basis.plus(self.S_L), axis = 2)
-        S_R_plus = np.average(basis.plus(self.S_R), axis = 2)
-        
-       # MSl = np.array([ np.matmul(conj(M),S_L_plus[:,:,n]) for n in range(nz) ])
-       # SrM = np.array([ np.matmul(S_R_plus[:,:,n],conj(M))  for n in range(nz) ])
-        MSl = np.array(np.matmul(dagger(self.M),S_L_plus))
-        SrM = np.array(np.matmul(S_R_plus,dagger(self.M)))
-        return (-1/self.p_abs)*(SrM-MSl)
-
-
-    #full Hamiltonian
-    def H(self, theta, phi):
-        return np.concatenate((np.concatenate( (self.H_R(theta, phi), np.conjugate(self.H_LR(theta,phi).transpose(1,0))), axis=0),
-                np.concatenate((self.H_LR(theta,phi), self.H_L(theta,phi)), axis = 0)), axis = 1)
-
-    #NEED DEFINITION OF DENSITY MATRIX
-    def P(self, theta, phi):
-        
-        return np.diag((1,0,0,0,0,0))
-
-
-
-
-    def resonance_full(self, theta, phi):
-        return gm.dotprod(self.H(theta,phi),self.P(theta,phi))
-    
-    #checks resonance condition [H_L]_f1f1 = [H_R]_f2f2
-    #equals resonance from tian et al if f1=f2=0 (electron)
-    def resonance_2f(self, theta, phi, f1, f2):
-        return self.H(theta,phi)[f1,f1]-self.H(theta,phi)[f2,f2]
-
-    def resonance_old(self, theta, phi):
-        return np.real(2**(-1/2)*G*self.n_b*(3.*self.Ye-np.ones_like(self.Ye))+self.S_R_kappa(theta,phi)[0,0])
-
-    def angularPlot(self, theta_res, phi_res, savefig=False, use_gm=True, direction_point=False):
-        
-        if use_gm==True:
-            H_LR_array = np.array([[np.abs(np.trace(self.H_LR(theta, phi))) 
-                                   for phi in np.linspace(0, 2*np.pi, phi_res)]
-                                   for theta in np.linspace(0, np.pi, theta_res)])
-        else: 
-            H_LR_array = np.array([[gm.sum_magnitude(self.H_LR(theta, phi)) 
-                                   for phi in np.linspace(0, 2*np.pi, phi_res)]
-                                   for theta in np.linspace(0, np.pi, theta_res)])
-
-        
-        resonance_array = np.array([[self.resonance_old(theta,phi)
-                                   for phi in np.linspace(0, 2*np.pi, phi_res)]
-                                   for theta in np.linspace(0, np.pi, theta_res)]) 
-
-        f = plt.figure()
-        ax = f.add_subplot(projection = 'mollweide')
-        ax.grid(False)
-        #ax.set_title(r'Angular plot of $H_{LR}$')
-        
-        H_LR_im = ax.pcolormesh(np.linspace(-np.pi, np.pi, phi_res), 
-                                np.linspace(0.5*np.pi, -0.5*np.pi, theta_res),
-                                H_LR_array, 
-                                cmap=plt.cm.hot, shading='auto')
-
-        plt.colorbar(H_LR_im, label="eV")
-
-        #resonance 
-        res_im = ax.contour(np.linspace(-np.pi, np.pi, phi_res),
-                            np.linspace(0.5*np.pi, -0.5*np.pi, theta_res),
-                           resonance_array, levels=[0.], colors='cyan', label = "Resonant Directions" )
-        
-        h1,l1 = res_im.legend_elements()
-
-        #add net flux point 
-        J_avg = np.array([gm.magnitude(np.average(self.J[n], axis = 2)) for n in range(0,4)])
-        flux_point = ax.scatter([np.arctan2(J_avg[2],J_avg[1])],[np.arctan2(J_avg[3],
-                                                (J_avg[1]**2+J_avg[2]**2)**(1/2))],  label = 'ELN Flux Direction', color='lime')
-        
-        #add (electron) neutrino direction point
-        
-        flow_direction = np.array(self.merger_grid['fn_a(1|ccm)'])[:,self.location[0],self.location[1],self.location[2]]
-        direction_point = ax.scatter([np.arctan2(flow_direction[1],flow_direction[0])],[np.arctan2(flow_direction[2], (flow_direction[0]**2+flow_direction[1]**2)**(1/2))],  label = 'Neutrino Flow Direction', color='magenta')
- 
-            
-        
-        
-        
-        plt.legend([h1[0], flux_point, direction_point], ["Resonant Directions", "Number Flux Direction", "Polar Direction"], loc=(0, 1.13))
-
-            
-        if savefig == True: 
-            plt.tight_layout()
-            plt.savefig('angularPlot.png', dpi=300)
-        
-         
-        
-    #uses scipy rootfinder to locate polar angle of resonance contour. Assumes rotational symmetry (picks phi=0)
-    #Currently only works for the old resonance condition
-    
-    #uses scipy rootfinder to locate polar angle of resonance contour. Assumes rotational symmetry (picks phi=0)
-    #Currently only works for the old resonance condition
-    
-    def J_avg(self): 
-        return np.array([gm.sum_magnitude(np.average(self.J[n], axis = 2)) for n in range(0,4)])
-    
-    def resonant_theta(self, phi=0):
-        res_function = self.resonance_old
-        return opt.bisect(res_function,0,np.pi,args = (phi))
-    
-    #resonant Hamiltionian at azimuthal angle phi (should be independent of phi)
-    def resonant_Hamiltonian(self, phi=0):
-        theta = self.resonant_theta(phi)
-        return self.H(theta,phi)
-    
-    def H_array(self):
-        return np.array([[gm.sum_magnitude(self.H_LR(theta, phi)) 
-                                   for phi in np.linspace(0, 2*np.pi, 50)]
-                                   for theta in np.linspace(0, np.pi, 50)])
-       
-        
         
 #generates plots vs time of spin parameters in fast flavor instability simulation
 class TimePlots:
@@ -661,50 +454,7 @@ def append_to_hdf5_scalar(outputfile, datasetname, data):
     scalar = gm.scalarfunc(data)
     append_to_hdf5(outputfile, datasetname, scalar)
 
-#total takes in read-only h5 dataset and the array you want to compute and just adds the real and imaginary parts for the array
-def total(d, array):
-    return (1+0*1j)*np.array(d[array+'R'])+1j*np.array(d[array+'I'])
 
-def J(d):
-    return total(d,'J(eV^3)')
-
-
-###################
-###################
-
-## Chiral Potentials ##
-# input: flux[spacetime, f1, f2, z]
-# output: Sigma_R[spacetime, f1, f2, z]
-def sigma(flux):
-        Sigma_R=0j*np.zeros(np.shape(flux)) 
-        Sigma_L=0j*np.zeros(np.shape(flux))
-        for n in range(0,4):
-                Sigma_R[n]=2**(1./2.)*G*(flux[n]+trace_matrix(flux[n]))
-        Sigma_L=(-1)*np.transpose(Sigma_R, axes=(0,2,1,3)) #for majorana 
-        return Sigma_R, Sigma_L
-
-## Mass Matrix ##	
-m23=np.array([[1,0*1j,0],
-              [0,np.cos(a23),np.sin(a23)],
-              [0,-np.sin(a23),np.cos(a23)]])
-
-m13=np.array([[np.cos(a13),0,np.sin(a13)*np.exp(-1j*delta)],
-              [0,1,0],
-              [-np.sin(a13)*np.exp(1j*delta),0,np.cos(a13)]])
-
-m12=np.array([[np.cos(a12),np.sin(a12),0],
-              [-np.sin(a12),np.cos(a12),0],
-              [0,0*1j,1]])
-
-m=m23 @ m13 @ m12
-#m is the mass mixing (MNS) matrix -- m*(1,2,3)=(e,mu,tau)
-M_mass_basis=([[m_1,0*1j,0],[0,m_2,0],[0,0,m_3]])
-M_3flavor = m @ M_mass_basis @ dagger(m) #(3,3)
-M=M_3flavor
-
-m2 = np.array([[np.cos(a12),np.sin(a12)],[-np.sin(a12),np.cos(a12)]])
-M_mass_basis_2 =([[m_1,0*1j],[0,m_2]])
-M_2flavor = m2 @ M_mass_basis_2 @ dagger(m2)
                                     
                 
 ## Non-Interacting Term ## [f1, f2]
@@ -843,10 +593,25 @@ def interact(d, outputfilename, basis_theta, basis_phi, time=0):
     
     
 # Input: what folder do we want to process?
-def interact_scalar(d, outputfilename, basis_theta, basis_phi):
+def interact_scalar(d, outputfilename, basis_theta, basis_phi, time=0):
     # Read in the data
     eds = emu.EmuDataset(d)
     nz = eds.Nz
+
+    #extract the data, depending on type
+    if d[-3:]=='.h5':
+        nz, t, z, J_p, J_a, J, nF = calculator_h5(d, outputfilename, time)
+    else:
+        nz, t, z, J_p, J_a, J, nF = calculator_eds(d, outputfilename)
+        
+    #define the size of the mass matrices
+    if nF == 2:
+        M=M_2flavor
+    elif nF == 3:
+        M=M_3flavor
+    else:
+        print("unsupported flavor number.")
+        return "unsupported flavor number."
 
     # open the hdf5 file
     outputfile = h5py.File(outputfilename, "a")
@@ -856,8 +621,8 @@ def interact_scalar(d, outputfilename, basis_theta, basis_phi):
 
     # write the free Hamiltonians
     if "H_R_free(eV)" not in outputfile:
-            outputfile["H_R_free(eV)"] = H_R_free
-            outputfile["H_L_free(eV)"] = H_L_free
+            outputfile["H_R_free(eV)"] = H_R_free(M)
+            outputfile["H_L_free(eV)"] = H_L_free(M)
 
     # write the z grid
     if "z(cm)" not in outputfile:
