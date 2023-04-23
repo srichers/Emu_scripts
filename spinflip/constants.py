e8019d5e18a0a7a758c143ca5089484e4b267c5b
Sherwood Richers <richers@utk.edu>
2023-04-23 19:03:43 -0400

    fix units of the speed of light (and small formatting tweaks)

--- spinflip/constants.py
+++ spinflip/constants.py
@@ -2,10 +2,10 @@ import numpy as np
 from matrix import dagger
 
 #fermi coupling constant: G/(c*hbar)^3=1.166 378 7(6)×10−5 GeV−2 --> G=1.166 378 7×10−23 eV^−2 (natural units:c=hbar=1)
-G=1.1663787*10**(-23) # eV^-2
-c=299792458         # cm/s
+G=1.1663787e-23       # eV^-2
+c=2.99792458e10       # cm/s
 hbar =6.582119569E-16 #eV s
-M_p=1.6726219*10**(-24)#grams (Proton mass)
+M_p=1.6726219e-24     #grams (Proton mass)
 
 #mixing angles (rad): (values from NuFit)
 a12=33.4*np.pi*2/360
