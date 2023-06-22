3ab24cd7edc3037229e6b96ac09ddf6af5a2d2ad
henrypurcell <henrypurcell88@gmail.com>
2023-06-22 15:30:09 -0400

    fixed old poorly written code in merger_grid, removed lots of redundant functions (especially adiabaticity stuff that was using non-relativistic gradients) fixed up contour_plot, largely the same thing but can plot for multiple z-slices and it looks better Added contour_plot to paper_plots.py

--- spinflip/paper_plots.py
+++ spinflip/paper_plots.py
@@ -13,6 +13,7 @@ import four_current as fc
 import matrix
 import diagonalizer as dg
 import time_plots as tp
+import merger_grid as mg
 import glob
 import concurrent
 import matplotlib as mpl
@@ -49,28 +50,52 @@ mpl.rcParams['ytick.minor.size'] = 4
 mpl.rcParams['ytick.minor.width'] = 2
 mpl.rcParams['axes.linewidth'] = 2
 mpl.rcParams['axes.grid'] = True
+plt.minorticks_on()
 
 
-
-
-
-location=[80,73,99]
-xmin = 75
-xmax = 83
-ymin = 67
-ymax = 77
-zmin = 97
-zmax = 99
-
+#####################
+###plot parameters###
+#####################
 merger_data_filename = "/mnt/scratch/shared/2-orthonormal_distributions/model_rl0_orthonormal_rotated.h5"
 emu_data_loc = "/mnt/scratch/shared/3-Henry_NSM_box/"
-emu_filename = emu_data_loc + "i{:03d}".format(location[0])+"_j{:03d}".format(location[1])+"_k{:03d}".format(location[2])+"/allData.h5"
 gradient_filename = "/mnt/scratch/shared/4-gradients/gradients_start.h5"
+
+#3 z-values of cross sections to plot
+zvals = [97,98,99]
+
+#location of specific point to analyze
+location=[80,73,zvals[1]] #will appear in middle plot in contour_plot hence zvals[1]
+emu_filename = emu_data_loc + "i{:03d}".format(location[0])+"_j{:03d}".format(location[1])+"_k{:03d}".format(location[2])+"/allData.h5"
+
+#boundaries of regions over which gradients have been computed (for adiabaticity analysis)
+#nth item in list corresponds to nth cross section (currently all are the same)
+xmins = [75,75,75]
+xmaxs = [83,83,83]
+ymins = [67,67,67]
+ymaxs = [77,77,77]
+
+#neutrino momentum
 p_abs = 1e7 # eV
 
 
 
-###Adiabaticity Plot: plots average value of adiabaticity on resonance band at each grid cell###
+###########
+###plots###
+###########
+
+### Resonance and FFI Regions Plot: plots resonance and FFI regions on z-cross sections ###
+mg.Merger_Grid(zvals, merger_data_loc=merger_data_filename, p_abs=p_abs
+               ).contour_plot(x=[None,location[0],None], 
+                              y=[None,location[1],None], 
+                              rect_xmin = xmins, rect_xmax = xmaxs,
+                              rect_ymin = ymins, rect_ymax = ymaxs,
+                              savefig = True)
+
+### Adiabaticity Plot: plots average value of adiabaticity on resonance band at each grid cell###
 #(averaging over azimuthal anglues, picking phi_resolution of these at each cell)
 #Plots for z-cross sections - first input is a list of z's to plot. We can do 3 cross sections for the paper
-sft.Gradients(gradient_filename, merger_data_filename).plotAdiabaticities([97,98,99],emu_data_loc, p_abs, 1E-8, 1E-7)
+sft.Gradients(gradient_filename, merger_data_filename
+              ).plotAdiabaticities(zvals, emu_data_loc, p_abs, 1E-8, 1E-7)
+
+### Single-Point Adiabaticity plot: shows adiabaticity and gradient vs azimuthal angle over resonance band for selected point ###
+SP = sft.SpinParams(1, emu_filename, merger_data_filename, location, p_abs, resonance_type = 'simplified')
