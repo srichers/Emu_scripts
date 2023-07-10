d2894a2481b69867bebfa90705f7293f4068f972
henrypurcell <henrypurcell88@gmail.com>
2023-07-10 00:26:09 -0400

    added argparse attributes to paper_plots including a call to lower the resolution of the plots so I can debug more easily. Edited some plot parameters in correspondence with changed values due to changing neutrino masses. Also fixed some bugs.

--- spinflip/paper_plots.py
+++ spinflip/paper_plots.py
@@ -23,6 +23,7 @@ import matplotlib.axes as ax
 from mpl_toolkits.mplot3d import Axes3D
 from four_current import four_current, read_gradients
 from matrix import visualizer
+import argparse
 #import latex
 from itertools import product
 
@@ -60,8 +61,19 @@ np.set_printoptions(precision=16)
 ###plot parameters###
 #####################
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-o", "--outputfile", help="where to store plots")
+parser.add_argument("-l", "--lowres", help="make plots lowres to debug quickly (still takes a while)", action="store_true")
+args = parser.parse_args()
+
 #location to save png's
-plot_output_file = "/mnt/scratch/henrypurcell/N3AS/spin-flip-plots/"
+plot_output_file =  args.outputfile
+
+#resolutions
+if args.lowres:
+    resolution = 20
+else:
+    resolution = 300
 
 merger_data_filename = "/mnt/scratch/shared/2-orthonormal_distributions/model_rl0_orthonormal_rotated.h5"
 emu_data_loc = "/mnt/scratch/shared/3-Henry_NSM_box/"
@@ -71,7 +83,7 @@ gradient_filename = "/mnt/scratch/shared/4-gradients/gradients_start.h5"
 zvals = [97,98,99]
 
 #location of specific point to analyze
-location=[80,73,zvals[1]] #will appear in middle plot in contour_plot hence zvals[1]
+location=[80,73,zvals[2]] #will appear in middle plot in contour_plot hence zvals[1]
 emu_filename = emu_data_loc + "i{:03d}".format(location[0])+"_j{:03d}".format(location[1])+"_k{:03d}".format(location[2])+"/allData.h5"
 
 #boundaries of regions over which gradients have been computed (for adiabaticity analysis)
@@ -99,7 +111,7 @@ mg.Merger_Grid(zvals,
                             y=[None,location[1],None], 
                             rect_xmin = xmins, rect_xmax = xmaxs,
                             rect_ymin = ymins, rect_ymax = ymaxs,
-                            savefig = plot_output_file + 'merger_grid_t0')
+                            savefig = plot_output_file + 'merger_grid_init')
 
 ### Adiabaticity Plot: plots average value of adiabaticity on resonance band at each grid cell###
 #(averaging over azimuthal anglues, picking phi_resolution of these at each cell)
@@ -109,8 +121,8 @@ sft.Gradients(gradient_filename,
               ).plotAdiabaticities(zvals,
                                    emu_data_loc, 
                                    p_abs,
-                                   1E-8, 1E-7,
-                                   savefig = plot_output_file + 'adiabaticity_grid_t0')
+                                   1E-11, 1E-7,
+                                   savefig = plot_output_file + 'adiabaticity_grid_init')
 
 
 
@@ -129,17 +141,17 @@ SP_0 = sft.SpinParams(0,
 
 
 ### Angular plot of Eigenvector Resonance value (1-|L-R|) and zoomed in version of the same ###
-SP_0.angularEigenvectorPlot(200, 200,
+SP_0.angularEigenvectorPlot(resolution, resolution,
                              value = 'lminusr',
                              phi_optimal = 3/4*np.pi,
-                             zoom = 0.1,
-                             shift = [-0.05,0],
-                             vmax = -4,
-                             zoom_resolution = 200,
+                             zoom = 0.08,
+                             shift = [-0.04,0],
+                             vmax = -4.5, vmin = -9,
+                             zoom_resolution = resolution,
                              initvector = 'default', 
                              method = 'Nelder-Mead',
                              bounds =[(np.pi/4, 3*np.pi/4)], 
-                             savefig= plot_output_file + 'angular_eigenvector_plot_t0',
+                             savefig= plot_output_file + 'angular_eigenvector_plot_init',
                              linearPlot = False)
 
 
@@ -147,42 +159,43 @@ SP_0.angularEigenvectorPlot(200, 200,
 ### Linear plot of Eigenvector resonance value vs theta for phi = 3/4 pi (shows 1D phi-slice of zoomed in picture from previous plot)###
 # this identifies resonance bands in the channels given in flavor-resonances with the given colors. There are 9 resonance bands total, and we can show 3
 # after showing 3 resonance bands we can plot their evolution in the next plot to show they are resonant.
-SP_0.linearEigenvectorPlot(300,  
+SP_0.linearEigenvectorPlot( 2*resolution,  
                             initvector = None, value = 'lminusr',
-                            zoom = 0.1, shift = -0.05, phi_optimal= np.pi,
+                            zoom = 0.08, shift = -0.04, phi_optimal= np.pi,
                             method = 'Nelder-Mead',
+                            vmax = 0.15,
                             extra_lines = None, extra_init_vectors = None,
-                            flavor_resonances = [(0,0,'cyan'), (1,1,'lime'), (0,1,'magenta')],
-                            savefig = plot_output_file + 'linear_eigenvector_plot_t0')
+                            flavor_resonances = [(0,0,'cyan'), (2,2,'lime'), (0,2,'magenta')],
+                            savefig = plot_output_file + 'linear_eigenvector_plot_init')
+                    
 
-  
 
 ### Compute resonant thetas on resonance bands for the next plot, and find the resonant angular width according to generalized resonance condition ###
-find_res_regions = SP_0.findResonantRegions(theta_resolution = 10000, 
+find_res_regions = SP_0.findResonantRegions(theta_resolution = 1000, 
                                             phi_optimal = np.pi,
-                                            min_dist_between_peaks = 2,
+                                            min_dist_between_peaks = 20,
                                             limits = [1.3, 1.4],
                                             resonance_threshold = 1 + 1/6 - np.sqrt(1 - (1/6)**2),
-                                            max_peak_count = 6,
+                                            max_peak_count = 3,
                                             method = 'Nelder-Mead',
-                                            makeplot = True)
+                                            makeplot = False,
+                                            printvalues = True)
 # print total resonant width that will be used in description of the plot
-resonant_width = find_res_regions['total width']
+resonant_width = find_res_regions['total_width']
 print('total initial resonant width = ', resonant_width)
-#compute thetas for next plot
-resonant_thetas = find_res_regions['max thetas']
 
 
 
 ### Diagnonalizer plot for identified resonance bands: shows evolution of resonant state for the resonant hamiltonians identified in the last plot
-# first compute the resonant vectors and Hamiltonians corresponding to these thetas
+# first compute the resonant thetas, vectors and Hamiltonians corresponding to these thetas
+resonant_thetas = find_res_regions['resonant_thetas']
 resonant_states = SP_0.resonant_states(resonant_thetas, P = True)
 resonant_Hs = np.array([SP_0.H(float(theta),np.pi) for theta in resonant_thetas])
 #make plot
-dg.multi_H_Plotter(resonant_Hs,
-                   t_lim_array = 'timescale',
-                   quantity_array = np.array([0,1,2,3,4,5]),
-                    resolution = 500,
+dg.multi_H_Plotter( resonant_Hs,
+                    t_lim_array = 'timescale',
+                    quantity_array = np.array([0,1,2,3,4,5]),
+                    resolution = resolution,
                     ylim = None,
                     init_state_array = resonant_states,
                     savefig = plot_output_file + 'mutli_H_plotter_init')
@@ -190,20 +203,18 @@ dg.multi_H_Plotter(resonant_Hs,
 
 
 ### Single-Point Adiabaticity plot: shows adiabaticity and gradient vs azimuthal angle over resonance band for selected point ###
-SP_0.azimuthalGradientsPlot(phi_resolution = 300,  
+SP_0.azimuthalGradientsPlot(phi_resolution = resolution,  
                             vmax = 5,
-                            savefig = plot_output_file + 'azimuthal_gradients_t0')
+                            savefig = plot_output_file + 'azimuthal_gradients_init')
 
 
 
 ### Adiabaticity width for single-point: prints and returns the angular width of the adiabatic region on the resonance band at the given point
 # We'll quote this value when describing the plot
-adiabatic_width = SP_0.findAdiabaticRegions(phi_resolution = 50,
+adiabatic_width = SP_0.findAdiabaticRegions(phi_resolution = resolution,
                           min_dist_between_peaks = 10,
                           adiabaticity_threshold = 1,
-                          max_peak_count = 3,
-                          savefig=False, 
-                          vmax = 1E-5,
+                          max_peak_count = 3, 
                           method = 'Nelder-Mead')
 
 print('total initial adiabatic width = ', adiabatic_width)
@@ -221,9 +232,18 @@ sft.multi_HLR_Plotter(
                     merger_data_filename,
                     location,
                     p_abs,
-                    300,
-                    300, 
+                    resolution,
+                    resolution, 
                     resonance_type = 'simplified',
                     savefig= plot_output_file + 'angular_hlr_init',
                     use_gm=True,
                     direction_point=False)
+
+
+
+### time plot of H_LR increasing during instability in z direction ###
+TP = tp.TimePlots([0,125], emu_filename,  merger_data_filename, location, p_abs)
+TP.plot( quantity = 'H_LR', 
+        avg_method = 'GM', 
+        theta=0, phi=0, 
+        savefig = plot_output_file + 'hlr_time_evolution')
